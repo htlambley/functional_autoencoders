810a4cc9c57ba19e9cd97e2f5e11043e65b7e27a
Hefin Lambley <hefin.lambley@warwick.ac.uk>
2026-08-05 12:32:02 +0100

    Update docstring for PoolingEncoder

--- src/functional_autoencoders/encoders/pooling_encoder.py
+++ src/functional_autoencoders/encoders/pooling_encoder.py
@@ -11,6 +11,9 @@ from functional_autoencoders.positional_encodings import (
 
 class MLPPointwiseOperator(nn.Module):
     mlp_dim: int = 128
+    # TODO: This name is misleading, as it is not the number of hidden layers, but the number of hidden layers + 1 (the output layer);
+    # see the definition of MLP. It is stated correctly in the paper, but the argument name is used throughout various config files
+    # here so we keep it to avoid breaking things. We should change it in the future.
     mlp_n_hidden_layers: int = 2
     positional_encoding: PositionalEncoding = IdentityEncoding()
 
@@ -39,27 +42,51 @@ class PoolingEncoder(Encoder):
 
     In function space, the encoder is the function-to-vector operation
 
-    $$f(u) = p \circ \rho \circ \mathrm{AGG} \circ F,$$
+    $$f(u) = \rho \circ \mathrm{AGG} \circ F,$$
 
     where:
-    - $p$ is a learnable linear projection to the latent dimension;
     - $\rho$ is a standard vector-to-vector feedforward neural network;
-    - $\mathrm{AGG}$ is a function-to-vector aggregation operator---in the original architecture, $\mathrm{AGG}(u) = \int_{\Omega} u(x) dx$;
-    - $F$ is a function-to-function operation---in the original architecture, a standard vector-to-vector neural network applied pointwise;
+    - $\mathrm{AGG}$ is a function-to-vector aggregation operator; and
+    - $F$ is a function-to-function operation.
 
-    TODO: customise rho, kappa architecture
-    TODO: customise discretisation of integral
+    This module allows $F$ to be specified using the `F` argument, $\mathrm{AGG}$ to be specified using the `aggregation` argument,
+    and parametrises $\rho$ as
+
+    $$ \rho(z) = W \tau(z) + b, $$
+
+    where $\tau$ can be specified using the `tau` argument, and $W$ and $b$ are learnable parameters with dimension
+    such that the output of the encoder has the correct dimension for the latent space.
+
+    To match the setup of the paper, set:
+    - `F` to `MLPPointwiseOperator()` with `mlp_dim=64` and `mlp_n_hidden_layers=3` (note that this means the MLP has 2 hidden layers plus an output layer, despite the name!),
+      and `positional_encoding=RandomFourierEncoding()` with a random matrix `B` of appropriate dimension (see quickstart notebook 2);
+    - `aggregation` to `MonteCarloIntegralAggregation()`; and
+    - `tau` to `IdentityMapping()`. 
+
+    This corresponds to the function-space operation
+
+    $$ f(u) = \rho \left( \int_{\Omega} \kappa\bigl(x, u(x)\bigr) \right),$$
+
+    where $\rho$ is a linear layer, $\kappa$ is a neural network with two hidden layers of width 64, output dimension 64, and the
+    integral is discretised as the Monte Carlo sum
+
+    $$ \int_{\Omega} \kappa\bigl(x, u(x)\bigr) \approx \frac{1}{N} \sum_{i=1}^{N} \kappa\bigl(x_i, u(x_i)\bigr).$$
+
+    Note that $\kappa$ uses a positional encoding of $x$ with random Fourier features.
+
+    By changing `aggregation`, it is possible to use other types of pooling, e.g., taking the $\max$ operation over the function, or
+    a softmax operation, for example.
     """
     latent_dim: int
     F: nn.Module = MLPPointwiseOperator()
     aggregation: nn.Module = MonteCarloIntegralAggregation()
-    rho: nn.Module = IdentityMapping()
+    tau: nn.Module = IdentityMapping()
 
     @nn.compact
     def __call__(self, u, x, train=False):
         u = self.F(u,x) 
         z = self.aggregation(u, x)
-        z = self.rho(z)
+        z = self.tau(z)
         d_out = self.latent_dim * 2 if self.is_variational else self.latent_dim
         z = nn.Dense(d_out)(z)
         return z
